9991a8ab6487b3b2059f83b5a9133532a06295db
Janae Hall <janaeelisa@gmail.com>
2019-10-13 06:45:27 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,12 +1,18 @@
 class Genre
   attr_reader :name
 
+  @@all = []
+
   def initialize(name)
     @name = name
+    @@all << self
   end
 
   def songs
     Song.all.select {|s| s.genre == self}
   end
 
+  def self.all
+    @@all
+  end
 end
