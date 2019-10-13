b9dadcb2933463da3fe87e11d63f3a21ee476a8c
Janae Hall <janaeelisa@gmail.com>
2019-10-13 07:03:25 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -15,4 +15,8 @@ class Patient
   def new_appointment(doctor, date)
     Appointment.new(self, date, doctor)
   end
+
+  def appointments
+    Appointment.all.select{|a| a.patient == self}
+  end
 end
